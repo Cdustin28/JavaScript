f493bea03d49d55e0d27c21ad67301ba7ede6c5b
Cole Dustin <coledustin28@gmail.com>
2022-12-19 15:53:56 -0600

    commit

--- input.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-
-var fname = prompt("What is your first name: ");
-var lname = prompt("What is your last name: ");
-
-console.log("Your full name is: ",fname,lname);
